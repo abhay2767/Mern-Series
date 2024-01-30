f6755005b4f9d582083c381dd3b220509e8bf62d
Abhay <abhaydubey2767@gmail.com>
2024-01-30 15:51:37 +0530

    done

--- React_Vite/vite.config.js
+++ React_Vite/vite.config.js
@@ -3,10 +3,10 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  /* server:{
+  server:{
     host:true,
     port:5173
-  }, */
+  }, 
   plugins: [react()],
   
 })
